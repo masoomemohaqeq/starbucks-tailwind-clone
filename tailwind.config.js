2a6e7049df1513259c6d854f027e0d29773b9e62
Mohaqeq_m <masoome.mohaqeq@outlook.com>
2023-01-11 21:04:18 +0330

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontWeight: {
+      hairline: 100,
+      thin: 200,
+      light: 300,
+      normal: 500,
+      medium: 600,
+      semibold: 700,
+      bold: 800,
+      extrabold: 900,
+      black: 900,
+    },
     extend: {
+      fontFamily: {
+        sans: ["sofia", ...defaultTheme.fontFamily.sans],
+      },
       boxShadow: {
         top: "-1px -2px 6px -3px rgba(79,79,79,0.50)",
+        hoverShadow: "0px -1px 0px 8px rgba(0,0,0,0.1);",
       },
       colors: {
         lightGreen: "#D4E9E2",
@@ -12,6 +30,7 @@ module.exports = {
         darkGreen: "#1E3932",
         mdManhattan: "#f4c5a1",
         lightGray: "#00000094",
+        darkBlack: "rgba(0,0,0,.85)",
       },
     },
   },
