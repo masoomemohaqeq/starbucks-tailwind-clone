5faca921c1561339ed7192954c007f01312cac3f
Mohaqeq_m <masoome.mohaqeq@outlook.com>
2023-01-12 01:16:36 +0330

    complete hamburger menu inner levels

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,19 @@
-var hamburgerToggler = document.getElementById("hamburger-toggler");
-var menuWrapper = document.getElementById("menu-wrapper");
+// Hamburger
+let hamburgerToggler = document.getElementById("hamburger-toggler");
+let menuWrapper = document.getElementById("menu-wrapper");
 
 hamburgerToggler.addEventListener("change", () => {
   hamburgerToggler.checked
     ? menuWrapper.classList.add("show")
     : menuWrapper.classList.remove("show");
 });
+
+// let closeInner = document.getElementsByName("close-inner-level");
+// let closeInnerText = document.getElementsByName("close-inner-level-text");
+
+// let innerLevelTogglers = document.getElementsByName("inner-level-toggler");
+
+// innerLevelTogglers.addEventListener("change", () => {
+//     this.checked
+//   this.closest();
+// });
