dc155fb33cf2d00f9f3d0198070528a9bfac0356
Mohaqeq_m <masoome.mohaqeq@outlook.com>
2023-03-12 21:07:56 +0330

    add disabled links, add fontawesome locally

--- src/js/index.js
+++ src/js/index.js
@@ -13,7 +13,7 @@ hamburgerToggler.addEventListener("change", () => {
   }
 });
 
-//Moving Animation for size
+//Moving Animation for product size
 let sizeRd = document.getElementsByName("size");
 let short = document.getElementById("short");
 let tall = document.getElementById("tall");
@@ -21,11 +21,12 @@ let grande = document.getElementById("grande");
 let venti = document.getElementById("venti");
 let movingAnim = document.getElementById("moving-anim");
 
+let left = 16.666667;
 sizeRd.forEach((rd) => {
   rd.addEventListener("change", () => {
-    short.checked && (movingAnim.style.left = "16.666667%");
-    tall.checked && (movingAnim.style.left = "33.333334%");
-    grande.checked && (movingAnim.style.left = "50%");
-    venti.checked && (movingAnim.style.left = "66.666668%");
+    short.checked && (movingAnim.style.left = `${left}%`);
+    tall.checked && (movingAnim.style.left = `${left * 2}%`);
+    grande.checked && (movingAnim.style.left = `${left * 3}%`);
+    venti.checked && (movingAnim.style.left = `${left * 4}%`);
   });
 });
